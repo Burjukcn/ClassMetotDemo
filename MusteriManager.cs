96e49c324446add4ac66eaa7d6b04401c6fc37f7
Burcu <Burcu@DESKTOP-IIES7V0>
2024-02-10 21:05:00 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle( Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler "+musteri.Name+" "+musteri.Surname+" isimli müşteri eklendi");
+        }
+
+        public void MusteriSil( Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler "+musteri.Name+" "+musteri.Surname+" isimli müşteri silindi");
+        }
+
+        public void MusteriListele(Musteri[] musteri) 
+        {
+            foreach( var Musteri in musteri ) 
+             {
+                Console.WriteLine("Müşteri İsmi: " + Musteri.Name);
+                Console.WriteLine("Müşteri Id: " + Musteri.ID  );
+                Console.WriteLine("Müşteri Soyisim: " + Musteri.Surname );
+                Console.WriteLine("Müşteri Açıklaması: " + Musteri.Description);
+                Console.WriteLine("--------------------");
+
+            }
+ 
+                
+                
+         }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir bankada müşteri takibi yapmak istiyorsunuz.
+            //Musteri isimli bir Class oluşturunuz.Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+            //MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+
+
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Name = "Nehir";
+            musteri1.Surname = "Çayan";
+            musteri1.Description = "Ado";
+
+            Musteri muster2= new Musteri();
+            muster2.ID = 2;
+            muster2.Name = "Ege";
+            muster2.Surname = "Çayan";
+            muster2.Description = "Ado";
+
+            Musteri musteri3= new Musteri();
+            musteri3.ID = 3;
+            musteri3.Name = "Ali";
+            musteri3.Surname = "Çayan";
+            musteri3.Description = "Adulte";
+
+
+            Musteri[] musteri = new Musteri[] { musteri1, muster2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("--------- Müşteri Ekleme Metotu------------");
+            musteriManager.MusteriEkle(musteri1);
+
+            Console.WriteLine("---------- Müşteri Silme Metotu -----------");
+            musteriManager.MusteriSil(musteri3 );
+
+            Console.WriteLine("---------- Müşteri Listeleme Metotu --------");
+            musteriManager.MusteriListele(musteri);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
